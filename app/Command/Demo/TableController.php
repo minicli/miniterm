ffc5ee1f6a633bfbf054669236c29ced890150d7
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-05-08 13:16:26 +0100

    Add Twig for creating view files

--- app/Command/Demo/TableController.php
+++ app/Command/Demo/TableController.php
@@ -16,20 +16,9 @@ class TableController extends CommandController
             $rows[] = [(string) $i, (string) rand(0, 10), "other string $i"];
         }
 
-        $html = '<table><thead><tr>';
-        foreach ($headers as $header) {
-            $html .= "<th>$header</th>";
-        }
-
-        $html .= '</tr></thead><tbody>';
-        foreach ($rows as $row) {
-            $html .= '<tr>';
-            foreach ($row as $cell) {
-                $html .= "<td>$cell</td>";
-            }
-            $html .= '</tr>';
-        }
-
-        render($html);
+        render(view('table.html', [
+            'headers' => $headers,
+            'rows' => $rows
+        ]));
     }
 }
