3c5480ec14502bf63aec32d33bf99b9ba1859e71
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-06-08 10:35:18 +0100

    Update test and default app signature

--- config/app.php
+++ config/app.php
@@ -10,10 +10,13 @@ return [
      * These are the core settings for your application.
      *****************************************************************************/
 
-    'app_name' => envconfig('MINICLI_APP_NAME', 'MiniTerm - MiniCLI Application Template powered with Termwind and Plates'),
+    'app_name' => envconfig(
+        'MINICLI_APP_NAME',
+        'MiniTerm - MiniCLI Application Template powered with Termwind and Plates'
+    ),
 
     'app_path' => [
-        __DIR__.'/app/Command',
+        __DIR__.'/../app/Command',
         '@minicli/command-help'
     ],
 
--- tests/Helpers.php
+++ tests/Helpers.php
@@ -6,48 +6,18 @@ use App\Config\TermwindOutputHandler;
 use App\Services\PlatesService;
 use App\Services\TermwindService;
 use Minicli\App;
-use Minicli\Command\CommandCall;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 use function Termwind\renderUsing;
 
-function getCommandsPath(): string
-{
-    return __DIR__.'/../app/Command';
-}
-
 function getApp(): App
 {
-    $config = [
-        'app_path' => getCommandsPath()
-    ];
-
-    $app = new App($config);
+    $app = new App();
     $app->addService('termwind', new TermwindService());
     $app->addService('plates', new PlatesService());
     $app->setOutputHandler(new TermwindOutputHandler());
 
     return $app;
-}
-
-function getProdApp(): App
-{
-    $config = [
-        'app_path' => getCommandsPath(),
-        'debug' => false
-    ];
-
-    $app = new App($config);
-    $app->addService('termwind', new TermwindService());
-    $app->addService('plates', new PlatesService());
-    $app->setOutputHandler(new TermwindOutputHandler());
-
-    return $app;
-}
-
-function getCommandCall(array $parameters = null): CommandCall
-{
-    return new CommandCall(array_merge(['minicli'], $parameters));
 }
 
 function getOutput(): BufferedOutput
